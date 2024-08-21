74af601598c319b49817b554d69f06b5752a10ce
JoseDanielCM <jodacarmon@gmail.com>
2024-08-21 11:19:11 -0500

    feat: :sparkles: pagina de especies con filtro, sin modal

--- especiesTraer.js
+++ especiesTraer.js
@@ -1,288 +1,152 @@
-const btnEspeciesall = document.getElementById("especies-all");
-const btnVehiculosall = document.getElementById("vehiculos-all");
-const btnNavesall = document.getElementById("naves-all");
-const resultContainer = document.getElementById("result-container")
+var paginaNum = 1
+let cargados = false
+
+const imagenesClasificacion = {
+    human_female:"clasificacion/humans.jpg  ",
+    artificial: "clasificacion/artificial.png",
+    sentient: "clasificacion/sentient.jpg",
+    mammal: "clasificacion/mammals.jpg",
+    gastropod: "clasificacion/gastropodo.jpg",
+    amphibian: "clasificacion/amphibian.jpg",
+    reptile: "clasificacion/reptile.jpg",
+    unknown: "clasificacion/unknown.jpg",
+    insectoid: "clasificacion/insectoid.jpg",
+    reptilian: "clasificacion/reptilian.jpg"
+};
+
+//async / await
+const peticion = async (url, opciones) => {
+    const respuesta = await fetch(url, opciones);
+    if (respuesta.ok) {
+        const datos = await respuesta.json()
+        return datos
+    } else {
+        return []
+    }
+}
+// Función asíncrona que controla el bucle del menú
+async function mostrarPersonajes(url,listaCategorias) {
+    let pagActual = url;
     
-const EspaciesAll = () => {
-    return new Promise((resolve,reject) => {
+    cargados = false
+    // RECORRER PAGINAS
+    while (pagActual!=null) {
+        paginaNum++
+        const principal_object = await peticion(pagActual);
+        pagActual = principal_object.next;  
+
+        let elementos_object = principal_object.results
         
-        const EspacieAll = new XMLHttpRequest();
-        EspacieAll.open("GET","https://swapi.py4e.com/api/species/");
-        EspacieAll.setRequestHeader("Content-Type","application.json");
-        setTimeout(() => {
-            EspacieAll.onload = function(){
-            if(EspacieAll.status === 200){
-                const response = JSON.parse(EspacieAll.responseText);
-                response.results.forEach(element => {
-                    const resultItem = document.createElement("div");
-                    resultItem.classList.add("result");
-                    resultItem.innerHTML = `
-                        <h3>${element.name}</h3>
-                    `;
-                    resolve(resultContainer.appendChild(resultItem));
-                });
-            }else{
-                console.log("Error :"+ EspacieAll.statusText);
-            } 
+        // RECORRER PERSONAJES 
+        // PAGINA ACTUAL
+        
+        elementos_object.forEach(async (item,index) => {
+            let nombre = item.name
+            let clasificacion = item.classification
             
-        }
-        EspacieAll.send();
-    },2000)
-    })
+            let average_height = item.average_height
+            let average_lifespan = item.average_lifespan
+            let language = item.language
+            
+            let imagenChar = imagenesClasificacion[clasificacion];
+            if (nombre=="Human") {
+                imagenChar = imagenesClasificacion.human_female
+            }
+            if (listaCategorias.includes(clasificacion)) {
+                let dataCharInner = `
+                    <div class="card" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        <div class="interno row g-0">
+                            <div class="col-sm-5">
+                                <img class="imgChar img-fluid rounded-start" src="${imagenChar}" alt="Darth Vader">
+                            </div>
+                            <div class="col-sm-7">
+                                <div class="card-body">
+                                    <h4 class="card-title">${nombre}</h4>
+                                    <p class="card-text"><strong>clasificacion: </strong>${clasificacion}</p>
+                                    <p class="card-text"><strong>height: </strong>${average_height}</p>
+                                    <p class="card-text"><strong>lifespan: </strong>${average_lifespan}</p>
+                                    <p class="card-text"><strong>language: </strong>${language}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    `
+
+                let cardContainer = document.createElement("section")
+                cardContainer.classList.add("col")
+
+                //*********************************** MODAL********************************** */
+                
+                let modalElement = document.getElementById('staticBackdrop');
+                let modal = new bootstrap.Modal(modalElement);
+
+                cardContainer.addEventListener("click",()=>{
+                    
+                    modal.show()
+                    document.getElementById("imgModal").src = imagenChar
+                    document.getElementById("titleModal").innerText = item.name
+                    document.getElementById("Climate").innerText = climate
+                    document.getElementById("terrain").innerText = item.terrain
+                    document.getElementById("population").innerText = item.population
+                    document.getElementById("Diameter").innerText = item.diameter
+                    document.getElementById("gravity").innerText = item.gravity
+                    document.getElementById("rotation_period").innerText = item.rotation_period
+                    document.getElementById("orbital_period").innerText = item.orbital_period
+            
+                })
+
+                document.getElementById('btnCerrar').addEventListener('click', () => {
+                modal.hide();
+                
+                });
+                // **********************************************************************************
+                cardContainer.innerHTML = dataCharInner
+
+                document.getElementById("personajesContenedor").appendChild(cardContainer)
+            }
+        });
+    
+        
+    }
+    cargados = true
+    paginaNum--
+
 }
 
+// Iniciar el menú
+const url = `https://swapi.dev/api/species/?page=${paginaNum}`;
 
-const VehiculosAll = () => {
-    return new Promise((resolve,reject) => {
-        
-        const VehiculoAll = new XMLHttpRequest();
-        VehiculoAll.open("GET","https://swapi.py4e.com/api/vehicles/");
-        VehiculoAll.setRequestHeader("Content-Type","application.json");
-        setTimeout(() => {
-            VehiculoAll.onload = function(){
-            if(VehiculoAll.status === 200){
-                const response = JSON.parse(VehiculoAll.responseText);
-                response.results.forEach(element => {
-                    const resultItem = document.createElement("div");
-                    resultItem.classList.add("result");
-                    resultItem.innerHTML = `
-                        <h3>${element.name}</h3>
-                    `;
-                    resolve(resultContainer.appendChild(resultItem));
-                });
-            }else{
-                console.log("Error :"+ VehiculoAll.statusText);
-            } 
+async function mostrarFiltrados(url) {
+    // RECORRER PAGINAS
+    
+    const listaBotonesPersonajes = document.querySelectorAll(".botonFiltro")
+    let listaCategorias = Object.keys(imagenesClasificacion)
+    mostrarPersonajes(url,listaCategorias)
+    listaBotonesPersonajes.forEach(element => {
+        element.addEventListener("click",()=>{
             
-        }
-        VehiculoAll.send();
-    },2000)
-    })
+            if (!cargados) {
+                alert("Los datos están cargando, espera...")
+                return
+            }
+            cargados=false
+            document.getElementById("personajesContenedor").innerHTML=""
+            
+            let cat = element.value
+            if (listaCategorias.includes(cat)) {
+                listaCategorias.splice(listaCategorias.indexOf(cat),1)
+                element.classList.remove("seleccionado")
+
+            } else {
+                listaCategorias.push(cat)
+                element.classList.add("seleccionado")
+            }
+            console.log(listaCategorias);
+            mostrarPersonajes(url,listaCategorias)
+        })
+    });
+    listaCategorias = []
+    
 }
 
-const NavesAll = () => {
-    return new Promise((resolve,reject) => {
-        
-        const NaveAll = new XMLHttpRequest();
-        NaveAll.open("GET","https://swapi.py4e.com/api/starships/");
-        NaveAll.setRequestHeader("Content-Type","application.json");
-        setTimeout(() => {
-            NaveAll.onload = function(){
-            if(NaveAll.status === 200){
-                const response = JSON.parse(NaveAll.responseText);
-                response.results.forEach(element => {
-                    const resultItem = document.createElement("div");
-                    resultItem.classList.add("result");
-                    resultItem.innerHTML = `
-                        <h3>${element.name}</h3>
-                    `;
-                    resolve(resultContainer.appendChild(resultItem));
-                });
-            }else{
-                console.log("Error :"+ NaveAll.statusText);
-            } 
-            
-        }
-        NaveAll.send();
-    },2000)
-    })
-}
-
-const ClasificacionEspecies = () => {
-    return new Promise((resolve,reject) => {
-        
-        const Clasificacion = new XMLHttpRequest();
-        Clasificacion.open("GET","https://swapi.py4e.com/api/species/");
-        Clasificacion.setRequestHeader("Content-Type","application.json");
-        setTimeout(() => {
-            Clasificacion.onload = function(){
-            if(Clasificacion.status === 200){
-                const response = JSON.parse(Clasificacion.responseText);
-                response.results.forEach(element => {
-                    const resultItem = document.createElement("div");
-                    resultItem.classList.add("result");
-                    resultItem.innerHTML = `
-                        <h3>${element.name}</h3> ⬇️ <p>${element.classification}</p>
-                    `;
-                    resolve(resultContainer.appendChild(resultItem));
-                });
-            }else{
-                console.log("Error :"+ Clasificacion.statusText);
-            } 
-            
-        }
-        Clasificacion.send();
-    },2000)
-    })
-}
-
-const LenguajesProvenientes = () => {
-    return new Promise((resolve,reject) => {
-        
-        const LenguajeProveniente = new XMLHttpRequest();
-        LenguajeProveniente.open("GET","https://swapi.py4e.com/api/species/");
-        LenguajeProveniente.setRequestHeader("Content-Type","application.json");
-        setTimeout(() => {
-            LenguajeProveniente.onload = function(){
-            if(LenguajeProveniente.status === 200){
-                const response = JSON.parse(LenguajeProveniente.responseText);
-                response.results.forEach(element => {
-                    const resultItem = document.createElement("div");
-                    resultItem.classList.add("result");
-                    resultItem.innerHTML = `
-                        <h3>${element.name}</h3> ⬇️ <p>${element.language}</p>
-                    `;
-                    resolve(resultContainer.appendChild(resultItem));
-                });
-            }else{
-                console.log("Error :"+ LenguajeProveniente.statusText);
-            } 
-            
-        }
-        LenguajeProveniente.send();
-    },2000)
-    })
-}
-
-
-const VehiculosCapacidad = () => {
-    return new Promise((resolve,reject) => {
-        
-        const VehiculoCapacidad = new XMLHttpRequest();
-        VehiculoCapacidad.open("GET","https://swapi.py4e.com/api/vehicles/");
-        VehiculoCapacidad.setRequestHeader("Content-Type","application.json");
-        setTimeout(() => {
-            VehiculoCapacidad.onload = function(){
-            if(VehiculoCapacidad.status === 200){
-                const response = JSON.parse(VehiculoCapacidad.responseText);
-                response.results.forEach(element => {
-                    const resultItem = document.createElement("div");
-                    resultItem.classList.add("result");
-                    resultItem.innerHTML = `
-                        <h3>${element.name}</h3> ⬇️ <p>${element.passengers} Pasajeros</p>
-                    `;
-                    resolve(resultContainer.appendChild(resultItem));
-                });
-            }else{
-                console.log("Error :"+ VehiculoCapacidad.statusText);
-            } 
-            
-        }
-        VehiculoCapacidad.send();
-    },2000)
-    })
-}
-
-const VehiculosFabricantes = () => {
-    return new Promise((resolve,reject) => {
-        
-        const VehiculoFabricante = new XMLHttpRequest();
-        VehiculoFabricante.open("GET","https://swapi.py4e.com/api/vehicles/");
-        VehiculoFabricante.setRequestHeader("Content-Type","application.json");
-        setTimeout(() => {
-            VehiculoFabricante.onload = function(){
-            if(VehiculoFabricante.status === 200){
-                const response = JSON.parse(VehiculoFabricante.responseText);
-                response.results.forEach(element => {
-                    const resultItem = document.createElement("div");
-                    resultItem.classList.add("result");
-                    resultItem.innerHTML = `
-                        <h3>${element.name}</h3> ⬇️ <p>Fabricado por: ${element.manufacturer}</p>
-                    `;
-                    resolve(resultContainer.appendChild(resultItem));
-                });
-            }else{
-                console.log("Error :"+ VehiculoFabricante.statusText);
-            } 
-            
-        }
-        VehiculoFabricante.send();
-    },2000)
-    })
-}
-
-const NavesLongitudes = () => {
-    return new Promise((resolve,reject) => {
-        
-        const Navelongitud = new XMLHttpRequest();
-        Navelongitud.open("GET","https://swapi.py4e.com/api/starships/");
-        Navelongitud.setRequestHeader("Content-Type","application.json");
-        setTimeout(() => {
-            Navelongitud.onload = function(){
-            if(Navelongitud.status === 200){
-                const response = JSON.parse(Navelongitud.responseText);
-                response.results.forEach(element => {
-                    const resultItem = document.createElement("div");
-                    resultItem.classList.add("result");
-                    resultItem.innerHTML = `
-                        <h3>${element.name}</h3> ⬇️ <p>${element.length} Metros</p>
-                    `;
-                    resolve(resultContainer.appendChild(resultItem));
-                });
-            }else{
-                console.log("Error :"+ Navelongitud.statusText);
-            } 
-            
-        }
-        Navelongitud.send();
-    },2000)
-    })
-}
-
-const TiposNaves = () => {
-    return new Promise((resolve,reject) => {
-        
-        const TipoNave = new XMLHttpRequest();
-        TipoNave.open("GET","https://swapi.py4e.com/api/starships/");
-        TipoNave.setRequestHeader("Content-Type","application.json");
-        setTimeout(() => {
-            TipoNave.onload = function(){
-            if(TipoNave.status === 200){
-                const response = JSON.parse(TipoNave.responseText);
-                response.results.forEach(element => {
-                    const resultItem = document.createElement("div");
-                    resultItem.classList.add("result");
-                    resultItem.innerHTML = `
-                        <h3>${element.name}</h3> ⬇️ <p>Clase: ${element.starship_class}</p>
-                    `;
-                    resolve(resultContainer.appendChild(resultItem));
-                });
-            }else{
-                console.log("Error :"+ TipoNave.statusText);
-            } 
-            
-        }
-        TipoNave.send();
-    },2000)
-    })
-}
-
-const CostosNaves = () => {
-    return new Promise((resolve,reject) => {
-        
-        const CostoNave = new XMLHttpRequest();
-        CostoNave.open("GET","https://swapi.py4e.com/api/starships/");
-        CostoNave.setRequestHeader("Content-Type","application.json");
-        setTimeout(() => {
-            CostoNave.onload = function(){
-            if(CostoNave.status === 200){
-                const response = JSON.parse(CostoNave.responseText);
-                response.results.forEach(element => {
-                    const resultItem = document.createElement("div");
-                    resultItem.classList.add("result");
-                    resultItem.innerHTML = `
-                        <h3>${element.name}</h3> ⬇️ <p>${element.cost_in_credits} Creditos</p>
-                    `;
-                    resolve(resultContainer.appendChild(resultItem));
-                });
-            }else{
-                console.log("Error :"+ CostoNave.statusText);
-            } 
-            
-        }
-        CostoNave.send();
-    },2000)
-    })
-}
-
-VehiculosAll()
+mostrarFiltrados(url)
